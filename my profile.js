d54160fbdbaa0660d7c472fe5dc92e545c3a9f64
natsuki kamada <59601051+natsukikamada@users.noreply.github.com>
2020-07-02 10:53:05 +0900

    Delete my profile.js

--- my profile.js
+++
@@ -1,6 +0,0 @@
-var hamburger = document.getElementById('Hamburger');
-var header_nav = document.getElementById('Header-Nav');
-
-hamburger.addEventListener('click', function () {
-    header_nav.classList.toggle("active");
-});
